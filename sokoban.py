901bd8add449ab3140325340399b37fff91c9b3e
sigma144 <sigma-144@hotmail.com>
2025-11-25 04:47:26 -0500

    New sokoban solver

--- sokoban.py
+++ sokoban.py
@@ -1,60 +1,32 @@
 
-from solver import Solver
-
-class SokobanState:
-    def __init__(self, board, x, y):
-        self.x = x; self.y = y
-        self.board = [row[:] for row in board]
-    def __hash__(self):
-        return hash(str(self.board))
-    def __eq__(self, state):
-        return self.x == state.x and self.y == state.y and str(self) == str(state)
-    def __repr__(self) -> str:
-        return '\n'.join([''.join(row) for row in self.board])
+from solvernew import Solver, GridState, DIRECTIONS
 
 class SokobanSolver(Solver):
-    def solve(self, board, target):
-        self.target = SokobanState(board, 0, 0)
-        start = SokobanState(target, 0, 0) #Solve in reverse 
-        for y in range(len(target)):
-            for x in range(len(target[y])):
-                if target[y][x] == 'P':
-                    start.x, start.y = x, y
-        for y in range(len(board)):
-            for x in range(len(board[y])):
-                if board[y][x] == 'P':
-                    self.target.x, self.target.y = x, y
-        print(start)
-        print(self.target)
-        move_list = self.solve_optimal(start)
-        #move_list = self.solve_optimal_debug(start)
-        for i in range(len(move_list)-1, -1, -1):
-            print(move_list[i])
-            input()
-            print('\n\n\n\n')
-        for i in range(-1, -len(move_list), -1):
-            dx = move_list[i].x - move_list[i-1].x
-            dy = move_list[i].y - move_list[i-1].y
-            if dx == -1: print("R", end="")
-            if dy == -1: print("D", end="")
-            if dx == 1: print("L", end="")
-            if dy == 1: print("U", end="")
-            if i % 10 == 0: print()
+    def setup(self, puzzle):
+        board, target = puzzle
+        self.target = GridState(board)
+        state = GridState(target) #Solve in reverse
+        state.x, state.y = state.find('P')
+        return state
     def get_next_states(self, state):
-        next = []
-        x = state.x; y = state.y
-        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        next = {}
+        #x, y = state.find_and_track('P')
+        x, y = state.x, state.y
+        for dir, (dx, dy) in DIRECTIONS.items():
             x2 = x + dx; y2 = y + dy
-            if state.board[y2][x2] == ' ':
-                new_state = SokobanState(state.board, x2, y2)
-                new_state.board[y2][x2] = 'P'
-                new_state.board[y][x] = ' '
-                next.append(new_state)
-                if state.board[y - dy][x - dx] == 'O':
-                    new_state = SokobanState(new_state.board, new_state.x, new_state.y)
-                    new_state.board[y][x] = 'O'
-                    new_state.board[y - dy][x - dx] = ' '
-                    next.append(new_state)
+            if state.get(x2, y2) == ' ':
+                new_state = state.copy()
+                #new_state.set_and_track(x2, y2, 'P')
+                new_state.set(x2, y2, 'P')
+                new_state.x, new_state.y = x2, y2
+                new_state.set(x, y, ' ')
+                next[dir] = new_state
+                if state.get(x-dx, y-dy) == 'O':
+                    new_state = new_state.copy()
+                    new_state.x, new_state.y = x2, y2
+                    new_state.set(x, y, 'O')
+                    new_state.set(x-dx, y-dy, ' ')
+                    next[dir+'P'] = new_state
         return next
     def check_finish(self, state):
         return state == self.target
@@ -91,7 +63,7 @@ def convert(l):
 testp, testt = convert([
     '#####',
     '# OX#',
-    '# P #',
+    '# PE#',
     '#####'
 ])
 
@@ -171,6 +143,26 @@ puzzle, target = convert([
     '######  #####',
     '#############',
 ])
+'''
+puzzle, target = convert([
+    '#######',
+    '###P ##',
+    '#     #',
+    '# #S#E#',
+    '# # OX#',
+    '#  S  #',
+    '##   ##',
+    '#######',
+])
+'''
 
-
-SokobanSolver().solve(puzzle, target)
+#import cProfile
+#import pstats
+#profiler = cProfile.Profile()
+#profiler.enable()
+#SokobanSolver().solve_optimal([testp, testt])
+#SokobanSolver().solve_optimal([puzzlex11, targetx11])
+SokobanSolver().solve_optimal([puzzle, target])
+#profiler.disable()
+#stats = pstats.Stats(profiler)
+#stats.sort_stats('cumulative').print_stats(10)  # Print top 10 stats
